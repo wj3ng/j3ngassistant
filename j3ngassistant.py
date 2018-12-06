1e67eb1630cc41c8d608a65075ffd8ed1b04350a
Willie Jeng <willie.jeng@gmail.com>
2018-12-05 21:38:09 -0800

    made voice recording into a class

--- j3ngassistant.py
+++ j3ngassistant.py
@@ -1,29 +1,13 @@
-import speechHandler
-import commandHandler
-
-
-assistantName = 'alexa'
-
-def listenForSpeech():
-
-    while True:
-
-        response = speechHandler.getSpeech()
-
-        if response['error'] is not None:
-            print('Speech not recognized.')
-        else:
-            transcription = response['transcription'].lower()
-            print('"' + transcription.replace(assistantName + ' ', assistantName.upper() + ' ', 1) + '"')
-            if assistantName + ' ' in transcription:
-                command = transcription[transcription.index(assistantName + ' ') + len(assistantName) + 1:]
-                commandHandler.parseCommand(command)
-                # deal with command
-
+from speechHandler import VoiceRecorder
 
 if __name__ == '__main__':
 
-    listenForSpeech()
+    vr = VoiceRecorder()
+    
+    vr.startListening()
+
+    input('press enter to end')
+    vr.stopListening()
 
     # print('What do you want me to say?')
     # speechHandler.gttsSay(input())
--- speechHandler.py
+++ speechHandler.py
@@ -4,54 +4,41 @@ from gtts import gTTS
 from io import BytesIO
 from pygame import mixer
 
-# returns object with success (bool), error (str), and transcription (str)
-def getSpeech(recognizer=sr.Recognizer(), microphone=sr.Microphone()): 
 
-    # check that recognizer and microphone arguments are appropriate type
-    if not isinstance(recognizer, sr.Recognizer):
-        raise TypeError("`recognizer` must be `Recognizer` instance")
-    
-    if not isinstance(microphone, sr.Microphone):
-        raise TypeError("`microphone` must be `Microphone` instance")
+class VoiceRecorder:
 
-    # adjust the recognizer sensitivity to ambient noise and record audio from the microphone
-    with microphone as source:
-        recognizer.adjust_for_ambient_noise(source)
-        audio = recognizer.listen(source)
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
 
-    # set up response object
-    response = {
-        "success": True,
-        "error": None,
-        "transcription": None
-    }
+    def __callback(self, r, audio):
 
-    # try recognizing the speech in the recording
-    # if a RequestError or UnknownValueError exception is caught, update the response object accordingly
-    try:
-        response["transcription"] = recognizer.recognize_google(audio)
-    except sr.RequestError:
-        # API was unreachable or unresponsive
-        response["success"] = False
-        response["error"] = "API unavailable"
-    except sr.UnknownValueError:
-        # speech was unintelligible
-        response["error"] = "Unable to recognize speech"
+        try:
+            transcription = r.recognize_google(audio)
+            print('"{0}"'.format(transcription))
+        except sr.UnknownValueError:
+            print('Could not understand audio.')
+        except sr.RequestError as e:
+            print('ERROR: {0}'.format(e))
 
-    return response
+    def startListening(self):
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source)
+        self.stopListening = self.recognizer.listen_in_background(self.microphone,self.__callback)
 
 
-# uses gTTS to turn text into speech in a mp3 file and play it
-def gttsSay(content, language="en", slow=False):
 
-    tts = gTTS(content,language,slow)
-    tts.save('tempTTS.mp3')
+# # uses gTTS to turn text into speech in a mp3 file and play it
+# def gttsSay(content, language='en', slow=False):
 
-    # maybe not a good idea to init every time?
-    mixer.init()
-    mixer.music.load('tempTTS.mp3')
-    mixer.music.play()
+#     tts = gTTS(content,language,slow)
+#     tts.save('tempTTS.mp3')
 
-    # waits until track finishes playing to exit the method
-    while mixer.music.get_busy():
-        pass
+#     # maybe not a good idea to init every time?
+#     mixer.init()
+#     mixer.music.load('tempTTS.mp3')
+#     mixer.music.play()
+
+#     # waits until track finishes playing to exit the method
+#     while mixer.music.get_busy():
+#         pass

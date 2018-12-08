532d735ef34022c1dc208239ff4a1b9f81350b41
Willie Jeng <willie.jeng@gmail.com>
2018-12-07 22:22:40 -0800

    put all code in single file

--- j3ngassistant.py
+++ j3ngassistant.py
@@ -1,16 +1,46 @@
-from speechHandler import VoiceRecorder
+from gtts import gTTS
+import speech_recognition as sr
+import pygame
+import os
 
-if __name__ == '__main__':
+def say_gtts(content, lang='en', slow=False):
+    """Plays Google TTS of a sentence"""
+    # TODO: use TemporaryFile instead of saving mp3 file
+    tts = gTTS(content, lang, slow)
+    tts.save('tempTTS.mp3')
+    pygame.mixer.music.load('tempTTS.mp3')
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy(): 
+        pygame.time.Clock().tick(10)
 
-    vr = VoiceRecorder()
-    
-    vr.startListening()
+def parse_command(transcription):
+    """Deals with said sentences"""
+    pass
 
-    input('press enter to end')
-    vr.stopListening()
+def listen_callback(r, audio):
+    """called when listening to voice in background"""
+    try:
+        transcription = r.recognize_google(audio)
+        print('"{0}"'.format(transcription))
+    except sr.UnknownValueError:
+        print('Could not understand audio.')
+    except sr.RequestError as e:
+        print('ERROR: {0}'.format(e))
 
-    # print('What do you want me to say?')
-    # speechHandler.gttsSay(input())
 
-    # while True:
-    #     print(speechHandler.getSpeech())
+# # initialize voice recording and speech recognition
+# recognizer = sr.Recognizer()
+# microphone = sr.Microphone()
+# 
+# # adjust for ambient noise (should make longer)
+# with microphone as source:
+    # recognizer.adjust_for_ambient_noise(source)
+# 
+# init pygame and pygame pygame.mixer
+pygame.init()
+pygame.mixer.init()
+
+say_gtts('hello world okay, playing despacito')
+
+# # starts listening in background, use stopListening() to stop
+# stopListening = recognizer.listen_in_background(microphone,listen_callback)
